38bce3e4b3cabfb30cde21570442166372c67844
Melvin Gruesbeck <mgruesbeck@gmail.com>
2017-04-20 08:59:51 -0700

    add pseudo code

--- sunfish.py
+++ sunfish.py
@@ -7,3 +7,11 @@ cl_s = CraigslistForSale(site='sfbay', category='boa', filters={'query': 'sunfis
 # loop thru results and print
 for result in cl_s.get_results(sort_by='newest'):
     print result
+
+'''
+1. run twice a day
+2. create an array of objects from query and write locally to data.json
+3. create variable which stores latest post date as current date
+4. if latest post date is newer than current date send email notification
+'''
+
